84614f3065235b01e4506a621b88e7d795e4df87
juliengauthier410 <juliengauthier410@penguin>
2023-05-09 11:17:23 +0200

    update menu.php -> menu antérieur

--- php/menu.php
+++ php/menu.php
@@ -34,8 +34,9 @@ ob_end_flush();
  *
  * @return int|string      string en cas d'erreur, int représentant la date affichée au format AAAAMMJJ sinon
  */
-function dateConsulteeL() : int|string {
-    if (!parametresControle('GET', [], ['jour', 'mois', 'annee'])){
+function dateConsulteeL(): int|string
+{
+    if (!parametresControle('GET', [], ['jour', 'mois', 'annee'])) {
         return 'Nom de paramètre invalide détecté dans l\'URL.';
     }
 
@@ -43,27 +44,27 @@ function dateConsulteeL() : int|string {
     list($jour, $mois, $annee) = getJourMoisAnneeFromDate(DATE_AUJOURDHUI);
 
     // vérification si les valeurs des paramètres reçus sont des chaînes numériques entières
-    foreach($_GET as $cle => $val){
-        if (! estEntier($val)){
+    foreach ($_GET as $cle => $val) {
+        if (!estEntier($val)) {
             return 'Valeur de paramètre non entière détectée dans l\'URL.';
         }
         // modification du jour, du mois ou de l'année de la date affichée
-        $$cle = (int)$val;
+        $$cle = (int) $val;
     }
 
-    if ($annee < 1000 || $annee > 9999){
+    if ($annee < 1000 || $annee > 9999) {
         return 'La valeur de l\'année n\'est pas sur 4 chiffres.';
     }
     if (!checkdate($mois, $jour, $annee)) {
         return "La date demandée \"$jour/$mois/$annee\" n'existe pas.";
     }
-    if ($annee < ANNEE_MIN){
-        return 'L\'année doit être supérieure ou égale à '.ANNEE_MIN.'.';
+    if ($annee < ANNEE_MIN) {
+        return 'L\'année doit être supérieure ou égale à ' . ANNEE_MIN . '.';
     }
-    if ($annee > ANNEE_MAX){
-        return 'L\'année doit être inférieure ou égale à '.ANNEE_MAX.'.';
+    if ($annee > ANNEE_MAX) {
+        return 'L\'année doit être inférieure ou égale à ' . ANNEE_MAX . '.';
     }
-    return $annee*10000 + $mois*100 + $jour;
+    return $annee * 10000 + $mois * 100 + $jour;
 }
 //_______________________________________________________________
 /**
@@ -73,20 +74,21 @@ function dateConsulteeL() : int|string {
  *
  * @return void
  */
-function affNavigationDateL(int $date): void{
+function affNavigationDateL(int $date): void
+{
     list($jour, $mois, $annee) = getJourMoisAnneeFromDate($date);
 
     // on détermine le jour précédent (ni samedi, ni dimanche)
     $jj = 0;
     do {
         $jj--;
-        $dateVeille = getdate(mktime(12, 0, 0, $mois, $jour+$jj, $annee));
+        $dateVeille = getdate(mktime(12, 0, 0, $mois, $jour + $jj, $annee));
     } while ($dateVeille['wday'] == 0 || $dateVeille['wday'] == 6);
     // on détermine le jour suivant (ni samedi, ni dimanche)
     $jj = 0;
     do {
         $jj++;
-        $dateDemain = getdate(mktime(12, 0, 0, $mois, $jour+$jj, $annee));
+        $dateDemain = getdate(mktime(12, 0, 0, $mois, $jour + $jj, $annee));
     } while ($dateDemain['wday'] == 0 || $dateDemain['wday'] == 6);
 
     $dateJour = getdate(mktime(12, 0, 0, $mois, $jour, $annee));
@@ -94,27 +96,27 @@ function affNavigationDateL(int $date): void{
 
     // affichage de la navigation pour choisir le jour affiché
     echo '<h2>',
-            $jourSemaine[$dateJour['wday']], ' ',
-            $jour, ' ',
-            getTableauMois()[$dateJour['mon']-1], ' ',
-            $annee,
+        $jourSemaine[$dateJour['wday']], ' ',
+        $jour, ' ',
+        getTableauMois()[$dateJour['mon'] - 1], ' ',
+        $annee,
         '</h2>',
 
         // on utilise un formulaire qui renvoie sur la page courante avec une méthode GET pour faire apparaître les 3 paramètres sur l'URL
         '<form id="navDate" action="menu.php" method="GET">',
-            '<a href="menu.php?jour=', $dateVeille['mday'], '&amp;mois=', $dateVeille['mon'], '&amp;annee=',  $dateVeille['year'], '">Jour précédent</a>',
-            '<a href="menu.php?jour=', $dateDemain['mday'], '&amp;mois=', $dateDemain['mon'], '&amp;annee=', $dateDemain['year'], '">Jour suivant</a>',
-            'Date : ';
+        '<a href="menu.php?jour=', $dateVeille['mday'], '&amp;mois=', $dateVeille['mon'], '&amp;annee=', $dateVeille['year'], '">Jour précédent</a>',
+        '<a href="menu.php?jour=', $dateDemain['mday'], '&amp;mois=', $dateDemain['mon'], '&amp;annee=', $dateDemain['year'], '">Jour suivant</a>',
+        'Date : ';
 
     affListeNombre('jour', 1, 31, 1, $jour);
     affListeMois('mois', $mois);
     affListeNombre('annee', ANNEE_MIN, ANNEE_MAX, 1, $annee);
 
-    echo    '<input type="submit" value="Consulter">',
+    echo '<input type="submit" value="Consulter">',
         '</form>';
-        // le bouton submit n'a pas d'attribut name. Par conséquent, il n'y a pas d'élément correspondant transmis dans l'URL lors de la soumission
-        // du formulaire. Ainsi, l'URL de la page a toujours la même forme (http://..../php/menu.php?jour=7&mois=3&annee=2023) quel que soit le moyen
-        // de navigation utilisé (formulaire avec bouton 'Consulter', ou lien 'précédent' ou 'suivant')
+    // le bouton submit n'a pas d'attribut name. Par conséquent, il n'y a pas d'élément correspondant transmis dans l'URL lors de la soumission
+    // du formulaire. Ainsi, l'URL de la page a toujours la même forme (http://..../php/menu.php?jour=7&mois=3&annee=2023) quel que soit le moyen
+    // de navigation utilisé (formulaire avec bouton 'Consulter', ou lien 'précédent' ou 'suivant')
 }
 
 //_______________________________________________________________
@@ -126,7 +128,8 @@ function affNavigationDateL(int $date): void{
  *
  * @return bool                     true si le restoU est ouvert, false sinon
  */
-function bdMenuL_connect(int $date, array &$menu, array &$plat_usager) : bool {
+function bdMenuL_connect(int $date, array &$menu, array &$repas): bool
+{
 
     // ouverture de la connexion à la base de données
     $bd = bdConnect();
@@ -135,7 +138,18 @@ function bdMenuL_connect(int $date, array &$menu, array &$plat_usager) : bool {
     $sql = "SELECT plID, plNom, plCategorie, plCalories, plCarbone
             FROM plat LEFT JOIN menu ON (plID=mePlat AND meDate=$date)
             WHERE mePlat IS NOT NULL OR plCategorie = 'boisson'";
-    $sql_repas = "SELECT rePlat FROM repas WHERE reDate=$date and reUsager={$_SESSION['usID']}";
+    $sql_repas = "SELECT plID FROM repas, plat WHERE rePlat=plID AND reDate=$date AND reUsager={$_SESSION['usID']}";
+    // $sql = "SELECT p.plID, p.plNom, p.plCategorie, p.plCalories, p.plCarbone
+    //         FROM plat p 
+    //         LEFT JOIN menu m ON (p.plID=m.mePlat AND m.meDate=$date)
+    //         WHERE m.mePlat IS NOT NULL OR p.plCategorie = 'boisson'
+    //         OR p.plID IN (
+    //             SELECT r.rePlat 
+    //             FROM repas r 
+    //             WHERE r.reDate=$date and r.reUsager={$_SESSION['usID']}
+    //         )";
+
+
     // envoi de la requête SQL
     $res = bdSendRequest($bd, $sql);
     $res_repas = bdSendRequest($bd, $sql_repas);
@@ -153,17 +167,20 @@ function bdMenuL_connect(int $date, array &$menu, array &$plat_usager) : bool {
 
 
     // tableau associatif contenant les constituants du menu : un élément par section
-    $menu = array(  'entrees'           => array(),
-                    'plats'             => array(),
-                    'accompagnements'   => array(),
-                    'desserts'          => array(),
-                    'boissons'          => array()
+    $menu = array(
+        'entrees' => array(),
+        'plats' => array(),
+        'accompagnements' => array(),
+        'desserts' => array(),
+        'boissons' => array()
     );
 
-    // tableau associatif contenant les plats du repas de l'utilisateur
+    // liste des identifiants des plats commandés par l'utilisateur
     $repas = array();
+    
+    
 
-    // parcours des ressources :
+    // parcours des ressources de menu :
     while ($tab = mysqli_fetch_assoc($res)) {
         switch ($tab['plCategorie']) {
             case 'entree':
@@ -184,6 +201,12 @@ function bdMenuL_connect(int $date, array &$menu, array &$plat_usager) : bool {
                 $menu['boissons'][] = $tab;
         }
     }
+
+    // parcours des ressources de repas :
+    while ($tab = mysqli_fetch_assoc($res_repas)) {
+        $repas[] = $tab['plID'];
+    }
+
     // libération des ressources
     mysqli_free_result($res);
     mysqli_free_result($res_repas);
@@ -192,26 +215,34 @@ function bdMenuL_connect(int $date, array &$menu, array &$plat_usager) : bool {
     return true;
 }
 
-function bdMenuL(int $date, array &$menu) : bool {
+
+/**
+ * Summary of bdMenuL
+ * @param mixed $date
+ * @param mixed $menu
+ * @return bool
+ */
+function bdMenuL(int $date, array &$menu): bool
+{
 
     // ouverture de la connexion à la base de données
     $bd = bdConnect();
 
     // Récupération des plats qui sont proposés pour le menu (boissons incluses, divers exclus)
     $sql = "SELECT plID, plNom, plCategorie, plCalories, plCarbone
-            FROM plat LEFT JOIN menu ON (plID=mePlat AND meDate=$date)
+            FROM plat LEFT JOIN menu ON (plID=mePlat AND meDate={$date})
             WHERE mePlat IS NOT NULL OR plCategorie = 'boisson'";
-    
+
     // envoi de la requête SQL
     $res = bdSendRequest($bd, $sql);
-    
+
 
     // Quand le resto U est fermé, la requête précédente renvoie tous les enregistrements de la table Plat de
     // catégorie boisson : il y en a NB_CAT_BOISSON
     if (mysqli_num_rows($res) <= NB_CAT_BOISSON) {
         // libération des ressources
         mysqli_free_result($res);
-    
+
         // fermeture de la connexion au serveur de base de  données
         mysqli_close($bd);
         return false; // ==> fin de la fonction bdMenuL()
@@ -219,11 +250,12 @@ function bdMenuL(int $date, array &$menu) : bool {
 
 
     // tableau associatif contenant les constituants du menu : un élément par section
-    $menu = array(  'entrees'           => array(),
-                    'plats'             => array(),
-                    'accompagnements'   => array(),
-                    'desserts'          => array(),
-                    'boissons'          => array()
+    $menu = array(
+        'entrees' => array(),
+        'plats' => array(),
+        'accompagnements' => array(),
+        'desserts' => array(),
+        'boissons' => array()
     );
 
     // parcours des ressources :
@@ -259,17 +291,17 @@ function bdMenuL(int $date, array &$menu) : bool {
  *
  * @param  array       $p               tableau associatif contenant les informations du plat en cours d'affichage
  * @param  string      $catAff          catégorie d'affichage du plat
- * @param  array       $plat_usager     tableau contenant les identifiants des plats commandés par l'utilisateur
+ * @param  array       $repas     tableau contenant les identifiants des plats commandés par l'utilisateur
  *
  * @return void
  */
-function affPlatL(array $p, string $catAff, array $plat_usager): void {
-    if ($catAff != 'accompagnements'){ //radio bonton
+function affPlatL_connect(array $p, string $catAff, array $repas): void
+{
+    if ($catAff != 'accompagnements') { //radio bonton
         $name = "rad$catAff";
         $id = "{$name}{$p['plID']}";
         $type = 'radio';
-    }
-    else{ //checkbox
+    } else { //checkbox
         $id = $name = "cb{$p['plID']}";
         $type = 'checkbox';
     }
@@ -277,16 +309,62 @@ function affPlatL(array $p, string $catAff, array $plat_usager): void {
     // protection des sorties contre les attaques XSS
     $p['plNom'] = htmlProtegerSorties($p['plNom']);
 
+    //affichage de $repas
+    // echo '<pre>';
+    // print_r($repas);
+    // echo '</pre>';
+
     // Ajouter l'attribut "checked" si le plat a été commandé par l'utilisateur
-    $is_checked = in_array($p['plID'], $plat_usager) ? 'checked' : '';
+    $is_checked = '';
+    if (array_search($p['plID'], $repas, true)===false) {
+        $is_checked = 'checked';
+    }
 
-    echo    '<input id="', $id, '" name="', $name, '" type="', $type, '" value="', $p['plID'], '" ',$is_checked,'>',
-            '<label for="', $id,'">',
-                '<img src="../images/repas/', $p['plID'], '.jpg" alt="', $p['plNom'], '" title="', $p['plNom'], '">',
-                $p['plNom'], '<br>', '<span>', $p['plCarbone'],'kg eqCO2 / ', $p['plCalories'], 'kcal</span>',
-            '</label>';
-
+    echo '<input id="', $id, '" name="', $name, '" type="', $type, '" value="', $p['plID'], '" ', $is_checked, '>',
+        '<label for="', $id, '">',
+        '<img src="../images/repas/', $p['plID'], '.jpg" alt="', $p['plNom'], '" title="', $p['plNom'], '">',
+        $p['plNom'], '<br>', '<span>', $p['plCarbone'], 'kg eqCO2 / ', $p['plCalories'], 'kcal</span>',
+        '</label>';
 }
+
+/**
+ * Affichage d'un des constituants du menu.
+ *
+ * @param  array       $p               tableau associatif contenant les informations du plat en cours d'affichage
+ * @param  string      $catAff          catégorie d'affichage du plat
+ * @param  array       $repas     tableau contenant les identifiants des plats commandés par l'utilisateur
+ *
+ * @return void
+ */
+function affPlatL(array $p, string $catAff): void
+{
+    if ($catAff != 'accompagnements') { //radio bonton
+        $name = "rad$catAff";
+        $id = "{$name}{$p['plID']}";
+        $type = 'radio';
+    } else { //checkbox
+        $id = $name = "cb{$p['plID']}";
+        $type = 'checkbox';
+    }
+
+    // protection des sorties contre les attaques XSS
+    $p['plNom'] = htmlProtegerSorties($p['plNom']);
+
+    //affichage de $repas
+    // echo '<pre>';
+    // print_r($repas);
+    // echo '</pre>';
+
+    // Ajouter l'attribut "checked" si le plat a été commandé par l'utilisateur
+
+    echo '<input id="', $id, '" name="', $name, '" type="', $type, '" value="', $p['plID'], '" >',
+        '<label for="', $id, '">',
+        '<img src="../images/repas/', $p['plID'], '.jpg" alt="', $p['plNom'], '" title="', $p['plNom'], '">',
+        $p['plNom'], '<br>', '<span>', $p['plCarbone'], 'kg eqCO2 / ', $p['plCalories'], 'kcal</span>',
+        '</label>';
+}
+
+
 
 //_______________________________________________________________
 /**
@@ -294,55 +372,77 @@ function affPlatL(array $p, string $catAff, array $plat_usager): void {
  *
  * @return void
  */
-function affContenuL(): void {
+function affContenuL(): void
+{
 
     $date = dateConsulteeL();
 
     // si dateConsulteeL() renvoie une erreur
-    if (is_string($date)){
-        echo    '<h4 class="center nomargin">Erreur</h4>',
-                '<p>', $date, '</p>',
-                (strpos($date, 'URL') !== false) ?
-                '<p>Il faut utiliser une URL de la forme :<br>http://..../php/menu.php?jour=7&mois=3&annee=2023</p>':'';
+    if (is_string($date)) {
+        echo '<h4 class="center nomargin">Erreur</h4>',
+            '<p>', $date, '</p>',
+            (strpos($date, 'URL') !== false) ?
+            '<p>Il faut utiliser une URL de la forme :<br>http://..../php/menu.php?jour=7&mois=3&annee=2023</p>' : '';
         return; // ==> fin de la fonction affContenuL()
     }
     // si on arrive à ce point de l'exécution, alors la date est valide
-    
+
     // Génération de la navigation entre les dates 
     affNavigationDateL($date);
 
     // menu du jour
     $menu = [];
-    $plat_usager = [];
-// si la session est ouverte
-    echo isset($_SESSION['usID']);
-    if (isset($_SESSION['usID'])){
-        $restoOuvert = bdMenuL_connect($date, $menu, $plat_usager);
-    }
-    else {
+    $repas = [];
+    // si la session est ouverte
+    if (isset($_SESSION['usID'])) {
+        $restoOuvert = bdMenuL_connect($date, $menu, $repas);
+    } else {
         $restoOuvert = bdMenuL($date, $menu);
     }
 
-    if (! $restoOuvert){
+    if (!$restoOuvert) {
         echo '<p>Aucun repas n\'est servi ce jour.</p>';
         return; // ==> fin de la fonction affContenuL()
     }
-    
+
     // titre h3 des sections à afficher
-    $h3 = array('entrees'           => 'Entrée',
-                'plats'             => 'Plat', 
-                'accompagnements'   => 'Accompagnement(s)',
-                'desserts'          => 'Fromage/dessert', 
-                'boissons'          => 'Boisson'
+    $h3 = array(
+        'entrees' => 'Entrée',
+        'plats' => 'Plat',
+        'accompagnements' => 'Accompagnement(s)',
+        'desserts' => 'Fromage/dessert',
+        'boissons' => 'Boisson'
     );
-    
+
     // affichage du menu
-    foreach($menu as $key => $value){
+    foreach ($menu as $key => $value) {
         echo '<section class="bcChoix"><h3>', $h3[$key], '</h3>';
-        foreach ($value as $p) {
-            affPlatL($p, $key, $plat_usager);
+        if (isset($_SESSION['usID'])) {
+            foreach ($value as $p) {
+                affPlatL_connect($p, $key, $repas);
+            }
+        } else {
+            foreach ($value as $p) {
+                affPlatL($p, $key);
+            }
         }
+
         echo '</section>';
     }
+    // // affichage du bouton de validation
+    // if (isset($_SESSION['usID'])) {
+    //     echo '<section>',
+    //         '<h3>Validation</h3>',
+    //         '<p class="attention">',
+    //             '<img src="../images/attention.png" alt="attention" width="50" height="50">',
+    //             'Attention, une fois la commande réalisée, il n\'est pas possible de la modifier.<br>',
+    //             'Toute commande non-récupérée sera majorée d\'une somme forfaitaire de 10 euros.',
+    //         '</p>',
+    //         '<p class="center">',
+    //             '<input type="submit" name="btnCommander" value="Commander">',
+    //             '<input type="reset" name="btnAnnuler" value="Annuler">',
+    //         '</p>',
+    //     '</section>';
+    // }
 
 }
